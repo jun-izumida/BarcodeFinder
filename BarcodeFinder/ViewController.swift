1df2c16b73f6213f59048f08179249ef7d58a944
Jun Izumida <gdoublezero00@gmail.com>
2020-10-30 21:28:41 +0900

    Initial Commit

---
+++ BarcodeFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeFinder
+//
+//  Created by jun on 10/30/20.
+//  Copyright © 2020 jp.co.agel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
